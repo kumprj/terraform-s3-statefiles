0ab5fca02af373242c4db1a8e86cab30c48851cf
Robert Kump <kumprj@miamioh.edu>
2024-11-30 15:56:12 -0500

    sample repo with statefile

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -1,8 +1,8 @@
 resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
   name = "terraform-state-lock-dynamo"
   hash_key = "LockID"
-  read_capacity = 20
-  write_capacity = 20
+  read_capacity = 10
+  write_capacity = 10
  
   attribute {
     name = "LockID"
